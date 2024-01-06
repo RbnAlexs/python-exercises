c06323363166e34df669e56db6afc9ae73cfcd19
Ruben Sanchez <rubensanchezcolin@gmail.com>
2024-01-05 23:03:46 -0600

    First commit from Visual Studio Code :D

---
+++ import random.py
@@ -0,0 +1,11 @@
+import random
+import my_module
+
+random_integer = random.randint(0,100)
+print(my_module.pi)
+print(random_integer)
+random_decimal = random.random()
+print(random_decimal)
+#What is the range of random_decimal? 0.0000000000 to 0.9999999999
+#What is the difference between randint and random? randint is inclusive of the last number, random is not
+#What is the range of random_integer? 0 to 100
